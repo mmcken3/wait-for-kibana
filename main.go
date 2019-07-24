93e2ac2c3bf01b245a8b8de9126ddd596274ee87
mmcken3 <mitchell.mckenzie95@gmail.com>
2019-07-23 22:21:09 -0400

    Adjust passing in of hostname

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/jimmysawczuk/try"
@@ -14,25 +13,22 @@ import (
 
 var timeout = 60 * time.Second
 var interval = 1 * time.Second
+var host string
 
 func init() {
 	flag.DurationVar(&timeout, "timeout", 60*time.Second, "total amount of time to try")
 	flag.DurationVar(&interval, "interval", 1*time.Second, "amount of time to wait between tries")
+	flag.StringVar(&host, "host", "http://localhost:5601", "host of kibana server")
 
 	flag.Parse()
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		log.Fatal("the kibana endpoint is a required argument")
-	}
-
-	endpoint := os.Args[1]
 	start := time.Now()
 
 	log.Printf("attempting to connect to kibana (trying for %s, %s between attempts)", timeout, interval)
 
-	if err := try.Try(connectToKibana(endpoint), timeout, interval); err != nil {
+	if err := try.Try(connectToKibana(host), timeout, interval); err != nil {
 		log.Fatal(err)
 	}
 
